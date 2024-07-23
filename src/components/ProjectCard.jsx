df0c5caf3e20c2f18a404bffbd5fde1771afba64
Hanna Kunitskaya <hanna_kunitskaya@gmail.com>
2024-07-23 12:05:41 +0200

    add project card

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const ProjectCard = ({ title, imageSrc, description, link }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const firstSentence = description.split('. ')[0] + '.';
+
+  return (
+    <div className='p-2 project-container'>
+      <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">
+        <h3>{title}</h3>
+        <div><img src={imageSrc} alt={title} className='img-responsive' /></div>
+        <p>{isExpanded ? description : firstSentence}</p>
+      </a>
+      <button onClick={handleToggle}>
+        {isExpanded ? 'Show Less' : 'Read More'}
+      </button>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React from 'react';
+import ProjectCard from '../components/ProjectCard';
 
 const Projects = () => {
-    return (
-        <>
-            <h2 className='center p-2'>My Projects</h2>
-            <div className="flex">
-                <div className='p-2'>
-                    <h3>Facebook</h3>
-                    <div><img src="https://bit.ly/3Ai9Wdq" alt="Facebook" className='img-responsive' /></div>
-                    <p>A social network to stay in touch with friends.</p>
-                </div>
-                <div className='p-2'>
-                    <h3>Email</h3>
-                    <div><img src="https://bit.ly/3Ai9Wdq" alt="Email" className='img-responsive' /></div>
-                    <p>A social network to stay in touch with friends.</p>
-                </div>
-            </div>
-        </>
-    )
-}
+  const projects = [
+    {
+      title: 'Todo-Tracker',
+      imageSrc: 'https://bit.ly/3Ai9Wdq',
+      description: `The ReactJS Todo Tracker website is a web application designed to help users manage their daily tasks efficiently. 
+                    It allows users to add, edit, and delete tasks, mark them as completed, and view the history of task changes. 
+                    Built using React, this application provides a simple and intuitive interface for tracking to-dos and organizing 
+                    daily activities, enhancing productivity through an easy-to-use task management system.`,
+      link: 'https://reactjs-todo-tracker.netlify.app/'
+    },
+    {
+      title: 'Nasa-Api-App',
+      imageSrc: 'https://bit.ly/3Ai9Wdq',
+      description: `The NASA API React App website utilizes NASA's APOD (Astronomy Picture of the Day), NeoWs (Near Earth Object Web Service), 
+                    and EPIC (Earth Polychromatic Imaging Camera) APIs. This application allows users to view daily astronomy pictures, 
+                    track near-Earth objects, and explore Earth images taken by the EPIC camera. 
+                    It provides an interactive and educational experience, showcasing various space-related data through
+                    a user-friendly React interface.`,
+      link: 'https://nasa-api-react-app.netlify.app/'
+    }
+  ];
 
-export default Projects
+  return (
+    <>
+      <h2 className='center p-2'>My Projects</h2>
+      <div className="flex center">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            imageSrc={project.imageSrc}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Projects;
