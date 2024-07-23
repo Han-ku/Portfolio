511ee2de89f92f65a542cba9ce898f28cedc68c1
Hanna Kunitskaya <hanna_kunitskaya@gmail.com>
2024-07-23 11:16:27 +0200

    add about me info

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
+import React from 'react';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home.js'
 import Projects from './pages/Projects.js'
 import Contact from './pages/Contact.js'
-import React, { useEffect, useState } from 'react';
 
 function App() {
-
-  const [fileContent, setFileContent] = useState('')
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    fetch('/text.txt')
-      .then(response => response.text())
-      .then(data => {
-        setFileContent(data)
-        setLoading(false)
-      })
-      .catch(error => {
-        console.error('Error fetching the text file:', error)
-        setFileContent('Failed to load content.')
-        setLoading(false);
-      })
-  }, [])
 
   return (
     <>
      <BrowserRouter>
-
       <nav className="flex">
         <div id="nav-brand">
           <h3>Who am I?</h3>
@@ -38,22 +20,6 @@ function App() {
           <span><Link to='/contact'>Contact</Link></span>
         </div>
       </nav>
-
-      <div className="flex header">
-        <div className="user-info">
-          <h1>Hanna Kunitskaya</h1>
-          {loading ? (
-            <div className="loadingState">
-              <i className="fa-solid fa-gear fa-spin"></i>
-            </div>
-          ) : (
-            <p>{fileContent}</p>
-          )}
-        </div>
-        <div className="user-photo">
-          <img src="" alt="" className="img-responsive img-circle" />
-        </div>
-      </div>
 
       <Routes>
          <Route path='/' element={<Home />}></Route>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+    const [fileContent, setFileContent] = useState('')
+    const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('/about_me/text.txt')
+      .then(response => response.text())
+      .then(data => {
+        setFileContent(data)
+        setLoading(false)
+      })
+      .catch(error => {
+        console.error('Error fetching the text file:', error)
+        setFileContent('Failed to load content.')
+        setLoading(false);
+      })
+  }, [])
+
     return (
         <>
-            <h2 className='center p-2'>Welcome to My Portfolio</h2>
-            <div className="flex flex-center p-2">
-                <h3 className='main-links'>
-                    <Link to='/projects'>Projects</Link>
-                </h3>
-                <h3 className='main-links'>
-                    <Link to='/contact'>Contact</Link>
-                </h3>
+            <div className="flex header">
+                <div className="user-info">
+                    <h1>Hanna Kunitskaya</h1>
+                    {loading ? (
+                    <div className="loadingState">
+                        <i className="fa-solid fa-gear fa-spin"></i>
+                    </div>
+                    ) : (
+                    <>
+                        <pre>{fileContent}</pre>
+                        <div className='cv_container'>
+                            <a href="/about_me/CV_Kunitskaya_Hanna.pdf" target="_blank">Open my CV</a>
+                            <a href="/about_me/CV_Kunitskaya_Hanna.pdf" download>Download my CV</a>
+                        </div>
+                    </>
+                    )}
+                </div>
             </div>
         </>
     )
